9bf9c20613dfaf8cf7d1abc9b9dc43596ca52fa4
Mercer Chen <13900410+mercerchen@users.noreply.github.com>
2024-08-08 06:00:21 -0400

    ckpting

--- model.py
+++ model.py
@@ -10,32 +10,76 @@ import torch.nn.functional as F
 class ModelConfig:
     vocab_size: int
     context_len: int = 256
-    n_embd: int = 384
+    # heads
+    head_size: int = 64
     n_head: int = 6
-    head_size: int = 384
-    block_size: int = 10
     n_layer: int = 6
     # hyperparam
     device: str = "cuda"
     dropout: float = 0.2
 
+    def __post_init__(self):
+        self.n_embd = self.n_head * self.head_size
+        
 
 class Head(nn.Module):
     def __init__(self, config: ModelConfig):
         super().__init__()
+        self.config = config
         self.key = nn.Linear(config.n_embd, config.head_size, bias=False)
         self.query = nn.Linear(config.n_embd, config.head_size, bias=False)
         self.value = nn.Linear(config.n_embd, config.head_size, bias=False)
         self.register_buffer(
-            "tril", torch.tril(torch.ones(config.block_size, config.block_size))
+            "tril", torch.tril(torch.ones(config.context_len, config.context_len))
         )
 
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x: Tensor):
-        B, T, C = x.shape
+        _, T, C = x.shape
+        assert T == self.config.context_len and C == self.config.n_embd, f"Input is of T: {T}, C: {C}" \
+            f" but should be of T: {self.config.context_len} and C: {self.config.n_embd}"
+        
+        k: Tensor = self.key(x)  # B, T, head_size
+        q: Tensor = self.query(x)
+
+        # attention scores
+        wei: Tensor = q @ k.transpose(-2, -1) * (k.shape[-1] ** -0.5)  # B, T, T
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(input=wei, dim=-1)
+        wei = self.dropout(wei) # B, T, T
+        
+        v = self.value(x) # (B, T, T) x (B, T, n_embd) -> (B, T, n_embd)
+        
+        return wei @ v
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(config.head_size) for _ in range(config.n_head)])
+        self.proj = nn.Linear(config.n_embd, config.n_embd)
+        self.dropout = nn.Dropout(config.dropout)
+            
+    def forward(self, x: Tensor):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        return self.dropout(self.proj(out))
+    
+
+class FeedForward(nn.Module):
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(config.n_embd, 4 * config.n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * config.n_embd, config.n_embd),
+            nn.Dropout(config.dropout)
+        )
+        
+    def forward(self, x: Tensor):
+        return self.net(x)
+    
+    
 class Block(nn.Module):
     pass
 
